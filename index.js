993584add868823dfad9c49a1bd27b209eac018e
Christian Baros <ctbaros@gmail.com>
2019-04-04 15:30:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,14 +9,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign ({}, object)
-  delete newObj.key;
-  return newObj;
-}
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object;
-}
 
